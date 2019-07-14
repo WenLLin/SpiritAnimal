4d17137c74b6242b2120e5d15de847b3df8ed33c
Wen Lin <wen.l.lin@outlook.com>
2019-07-14 12:35:26 -0400

    Initial Commit

---
+++ SpiritAnimal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace OptionalPar
+{
+    class webSurvey
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What Is Your Spirit Animal? Take The Quiz To Find Out!");            
+            string[] arr = new string[7];
+            webSurvey.SpiritAnimal(arr, 0);            
+        }
+
+
+        static void SpiritAnimal(string[] animal, int yearOfBirth)
+        {          
+            //Randomly select an animal from the array
+            Random randAnimal = new Random();            
+            animal = new string[] { "Wolverine", "Ant-man", "Thanksgiving Turkey", "Ninja Turtle", "Black Panther", "Batman", "hot dog" };
+            int index = randAnimal.Next(animal.Length);
+            Console.Write("What year were you born:  ");
+            yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            //Calculate current age
+            var age = (2019 - yearOfBirth);
+            Console.WriteLine($"You are a {age} years old {animal[index]}");
+
+            Console.WriteLine("Do you want to try again? yes/no  ");
+            bool again = true;
+            do
+            {
+                index = randAnimal.Next(animal.Length);
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    Console.Write("What year were you born:  ");
+                    yearOfBirth = Convert.ToInt32(Console.ReadLine());                    
+                    age = (2019 - yearOfBirth);
+                    Console.WriteLine($"You are a {age} years old {animal[index]}");
+
+                    Console.WriteLine("Do you want to try again? yes/no  ");
+                }
+                else if (answer == "no")
+                {
+                    Console.WriteLine("Thank you, bye bye.");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please type in yes or no");
+                }
+            } while (again == true);
+        }
+        /*
+        static void SpiritAnimal(string[] animal)
+        {
+            animal = new string[] {"bear", "fox", "Turkey", "turtle","panther","deer","hot dog"};
+            SpiritAnimal(animal);
+        }*/
+    }
+}
